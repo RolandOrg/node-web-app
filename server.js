20df56ee39cb77ffaf8964fdc1889cf8761859ce
alyssa <barcia@us.ibm.com@Rolands-MacBook-Pro-2.local>
2020-04-08 15:54:29 -0400

    Hey

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Roland !!!!!!!!  v 1.0.2 ');
+  res.send('Hello Roland !!!!!!!!  v 1.0.3 ');
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log('Running on http://${HOST}:${PORT}');
