12849762a0ee692c46ca6fba3d28af4b14c21fcd
alyssa <barcia@us.ibm.com@Rolands-MacBook-Pro-2.local>
2020-04-09 16:22:34 -0400

    1.0.6

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Roland Barcia, you rock !!!!!  v 1.0.6 ');
+  res.send('Hello Roland Barcia, you rock !!!!!!  v 1.0.6 ');
 });
 
 app.listen(PORT, HOST);
