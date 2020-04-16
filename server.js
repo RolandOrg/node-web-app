68805b9eb71c768d176855f1605aaf66bb2ed1b8
alyssa <barcia@us.ibm.com@Rolands-MacBook-Pro-2.local>
2020-04-15 20:33:32 -0400

    1.1.6

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello CNCF, new pipleine, 1.1.6 !!!!!!  Latest Yeah ');
+  res.send('Hello FastStart, new pipleine, 1.1.7 !!!!!!  Latest Yeah ');
 });
 
 app.listen(PORT, HOST);
